ed4ca514608c49340e3c8bc18b07839c7c31a757
Michael Richens <michael.richens@gmail.com>
2023-04-26 21:10:01 +0100

    Converted site to use next.js

---
+++ pages/about-us.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Page from "../components/Page"
+
+const AboutUs = () => {
+	return (
+		<Page title="About Us">
+			<p>Learn more about us and our mission.</p>
+		</Page>
+	)
+}
+
+export default AboutUs
--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,41 @@
-const webpack = require('webpack');
-const dotenv = require('dotenv');
-const fs = require('fs');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-function loadEnvVariables() {
-  const env = dotenv.config().parsed;
-  const envFile = `.env.${process.env.NODE_ENV}`;
-  const envLocalPath = path.resolve(__dirname, '.env.local');
-  const envLocalExists = fs.existsSync(envLocalPath);
-
-  if (fs.existsSync(envFile)) {
-    const envConfig = dotenv.parse(fs.readFileSync(envFile));
-    for (const key in envConfig) {
-      env[key] = envConfig[key];
-    }
-  }
-
-  if (envLocalExists) {
-    const envLocal = dotenv.parse(fs.readFileSync(envLocalPath));
-    for (const key in envLocal) {
-      env[key] = envLocal[key];
-    }
-  }
-
-  return env;
-}
-
-
+const webpack = require("webpack")
+const fs = require("fs")
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(loadEnvVariables()),
-    }),
-  ],  
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    compress: true,
-    port: 3000,
-    open: true,
-    historyApiFallback: true,
-  }
-};
+	entry: "./src/index.js",
+	output: {
+		path: path.join(__dirname, "/dist"),
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html"
+		})
+	],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "dist")
+		},
+		compress: true,
+		port: 3000,
+		open: true,
+		historyApiFallback: true
+	}
+}
