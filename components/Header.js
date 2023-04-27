f5d3474f2040dc9c541c9246b2005d088f48302c
Michael Richens <michael@2ndchancepc.co.uk>
2023-04-27 10:09:50 +0100

    set up prettier for project with basic settings

--- components/Header.js
+++ components/Header.js
@@ -1,38 +1,32 @@
-import React from "react"
-import { useRouter } from "next/router"
-import Link from "next/link"
+import React from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const Header = () => {
-	const router = useRouter()
+  const router = useRouter()
 
-	const isActive = href => router.pathname === href
+  const isActive = (href) => router.pathname === href
 
-	return (
-		<header>
-			<nav>
-				<ul>
-					<li>
-						<Link
-							href="/"
-							className={isActive("/") ? "active-link" : ""}
-						>
-							Home
-						</Link>
-					</li>
-					<li>
-						<Link
-							href="/about-us"
-							className={
-								isActive("/about-us") ? "active-link" : ""
-							}
-						>
-							About Us
-						</Link>
-					</li>
-				</ul>
-			</nav>
-		</header>
-	)
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/' className={isActive('/') ? 'active-link' : ''}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href='/about-us'
+              className={isActive('/about-us') ? 'active-link' : ''}>
+              About Us
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
