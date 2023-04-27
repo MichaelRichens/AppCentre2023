a62a01c486a4bc555c18b7e2d09e31df71cd6cf7
Michael Richens <michael@2ndchancepc.co.uk>
2023-04-27 16:48:58 +0100

    added comment

--- components/NavLink.js
+++ components/NavLink.js
@@ -1,3 +1,6 @@
+/**
+ * Returns the passed element wrapped link if the current page does not match the passed href value, and unchanged if it does.
+ */
 import React from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
