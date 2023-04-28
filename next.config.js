a73b697714258d8751c11037f90e14523f9c4eae
Michael Richens <michael@2ndchancepc.co.uk>
2023-04-28 09:37:32 +0100

    netlify config changes

--- next.config.js
+++ next.config.js
@@ -1,27 +1,8 @@
 module.exports = {
   env: {
-    NEXT_PUBLIC_SITE_URL:
-      process.env.NEXT_PUBLIC_SITE_URL ||
-      process.env.DEPLOY_PRIME_URL ||
-      'http://localhost:3000',
-    NEXT_NETLIFY_SUBDOMAIN_URL:
-      process.env.NEXT_NETLIFY_SUBDOMAIN_URL ||
-      process.env.DEPLOY_PRIME_URL ||
-      'http://localhost:3000',
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+    NEXT_NETLIFY_SUBDOMAIN_URL: process.env.NEXT_NETLIFY_SUBDOMAIN_URL,
     DEPLOY_PRIME_URL: process.env.DEPLOY_PRIME_URL || 'http://localhost:3000',
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/.next' : '',
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=0, must-revalidate',
-          },
-        ],
-      },
-    ]
-  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/_next' : '',
 }
