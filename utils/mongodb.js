0ce30656c7dda7d786aacc08b31a8829a7099f69
Michael Richens <michael@2ndchancepc.co.uk>
2023-04-28 14:40:46 +0100

    debug logging

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -10,10 +10,13 @@ let cachedClient = null
 
 export async function connectToDatabase() {
   if (cachedClient) {
+    console.log('cached client')
     return { client: cachedClient }
   }
-
+  console.log('requesting from mongodb')
   const client = await MongoClient.connect(uri, options)
+  console.log('client')
+  console.log(client)
   cachedClient = client
   return { client }
 }
