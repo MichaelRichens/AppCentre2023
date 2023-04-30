e5593e5c4823ab41e3f588f96533c3eb7818f6da
Michael Richens <michael.richens@gmail.com>
2023-04-30 17:26:02 +0100

    more error fiddling

--- utils/fetchAndProcessProducts.js
+++ utils/fetchAndProcessProducts.js
@@ -1,16 +1,17 @@
 async function fetchProducts() {
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_DEPLOY_URL}/api/productsz`
+      `${process.env.NEXT_PUBLIC_DEPLOY_URL}/api/products`
     )
     if (!res.ok) {
-      console.error(`Error fetching products: ${res.status} ${res.statusText}`)
-      return []
+      throw new error(
+        `Error fetching products: ${res.status} ${res.statusText} ${process.env.NEXT_PUBLIC_DEPLOY_URL}/api/products`
+      )
     }
     const data = await res.json()
     return data
   } catch (error) {
-    throw new Error('Failed to fetch products.')
+    throw new Error('Failed to connect to products api.')
   }
 }
 
