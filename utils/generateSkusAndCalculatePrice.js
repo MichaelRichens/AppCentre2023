82b443445411e4bd5666e44fa1f972cd691349b3
Michael Richens <michael.richens@gmail.com>
2023-05-01 10:32:47 +0100

    comment

--- utils/generateSkusAndCalculatePrice.js
+++ utils/generateSkusAndCalculatePrice.js
@@ -3,7 +3,7 @@
  *
  * @param {Array} productSkus - The product skus to calculate price from.
  * @param {Object} configuratorOptions - The configurator options, such as type, users, and years.
- * @returns {Object} An object with the calculated price in the `price` field, and a skus field that contains an object that has a field for each sku with the value being the quantity of that sku.
+ * @returns {Object} An object with the calculated price in the `price` field, and a `skus` field that contains an object that has a field for each sku with the value being the quantity of that sku.
  */
 function generateSkusAndCalculatePrice(productSkus, configuratorOptions) {
   //Placeholder - price calculation logic here
