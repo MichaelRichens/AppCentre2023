dff043cfc8b212fa634a5118886e130c4c8e34c8
Michael Richens <michael.richens@gmail.com>
2023-05-01 12:27:34 +0100

    basic price calculation

--- utils/generateSkusAndCalculatePrice.js
+++ utils/generateSkusAndCalculatePrice.js
@@ -6,8 +6,7 @@
  * @returns {Object} An object with the calculated price in the `price` field, and a `skus` field that contains an object that has a field for each sku with the value being the quantity of that sku.
  */
 function generateSkusAndCalculatePrice(products, configuratorOptions) {
-  const options = { ...configuratorOptions }
-
+  const numUsers = configuratorOptions.users
   let price = 0
   const skus = {}
 
@@ -15,12 +14,42 @@ function generateSkusAndCalculatePrice(products, configuratorOptions) {
     (sku) => sku.years == configuratorOptions.years
   )
 
-  console.log(options)
-  productsWithCorrectYear.forEach((product) => {
-    // Your code here
-  })
+  console.log(configuratorOptions)
+  // We are relying on products (and therefore productsWithCorrectYear) being passed in already sorted by low to high user tiers.
+  for (let i = productsWithCorrectYear.length - 1; i >= 0; i--) {
+    const product = productsWithCorrectYear[i]
+    console.log(product)
+    console.log(product.units_from)
+    let unitsFrom
+    if (Number.isInteger(product.units_from) && product.units_from > 0) {
+      unitsFrom = product.units_from
+    } else {
+      unitsFrom = parseInt(process.env.NEXT_PUBLIC_DEFAULT_MIN_USERS, 10)
+    }
 
-  return { price: price, skus: skus, type: options.type, years: options.years }
+    let unitsTo
+    if (Number.isInteger(product.units_to) && product.units_to > 0) {
+      unitsTo = product.units_to
+    } else {
+      unitsTo = parseInt(process.env.NEXT_PUBLIC_DEFAULT_MAX_USERS, 10)
+    }
+
+    if (unitsFrom <= numUsers && numUsers <= unitsTo) {
+      console.log(
+        `${unitsFrom} <= ${numUsers} <= ${unitsTo} with price: ${product.price}`
+      )
+      skus[product.sku] = numUsers
+      price += product.price * numUsers
+      break
+    }
+  }
+
+  return {
+    price: price,
+    skus: skus,
+    type: configuratorOptions.type,
+    years: configuratorOptions.years,
+  }
 }
 
 export default generateSkusAndCalculatePrice
