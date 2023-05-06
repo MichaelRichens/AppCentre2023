d5f1eda6d3199b702648f93cdc650f912507fd6f
Michael Richens <michael.richens@gmail.com>
2023-05-06 23:15:49 +0100

    roughed out api route

---
+++ pages/api/store-configuration.js
@@ -0,0 +1,16 @@
+// pages/api/store-configuration.js
+import { isProductConfiguration } from '../../utils/typeCheck'
+
+export default async function handler(req, res) {
+	if (req.method === 'POST') {
+		const { formData, clientSideConfiguration } = req.body
+
+		if (formData !== null && clientSideConfiguration !== null && isProductConfiguration(clientSideConfiguration)) {
+			res.status(200).json({ message: 'Valid objects received' })
+		} else {
+			res.status(400).json({ message: 'Invalid objects received' })
+		}
+	} else {
+		res.status(405).json({ message: 'Method not allowed' })
+	}
+}
