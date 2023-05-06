514d7b5782b14552c69b7beb02e1521edf76dedc
Michael Richens <michael.richens@gmail.com>
2023-05-06 12:23:16 +0100

    Added some styling

--- components/Footer.js
+++ components/Footer.js
@@ -3,17 +3,19 @@ import React from 'react'
 const Footer = () => {
 	return (
 		<footer>
-			<p>
-				© 2006 - {new Date().getFullYear()} AppCentre. AppCentre is a part of{' '}
-				<a
-					href='https://www.macupgrades.co.uk/store/'
-					target='_blank'
-					rel='noopener noreferrer'>
-					MacUpgrades
-				</a>{' '}
-				/ Second Chance PC Ltd. Company number 4331031. Registered for VAT GB
-				783705210.
-			</p>
+			<div id='footerInnerWrapper'>
+				<p>
+					© 2006 - {new Date().getFullYear()} AppCentre. AppCentre is a part of{' '}
+					<a
+						href='https://www.macupgrades.co.uk/store/'
+						target='_blank'
+						rel='noopener noreferrer'>
+						MacUpgrades
+					</a>{' '}
+					/ Second Chance PC Ltd. Company number 4331031. Registered for VAT GB
+					783705210.
+				</p>
+			</div>
 		</footer>
 	)
 }
--- components/Header.js
+++ components/Header.js
@@ -1,36 +1,39 @@
 import React from 'react'
 import NavLink from './NavLink'
+import styles from '../styles/Header.module.css'
 
 const Header = () => {
 	return (
-		<header>
-			<div id='headerNavContainer'>
-				<nav aria-label='Primary navigation'>
-					<ul>
-						<li>
-							<NavLink href='/'>Home</NavLink>
-						</li>
-						<li>
-							<NavLink href='/about-us'>About Us</NavLink>
-						</li>
-					</ul>
-				</nav>
-				<nav aria-label='Product navigation'>
-					<ul>
-						<li>
-							<NavLink href='/kerio-connect'>Kerio Connect</NavLink>
-						</li>
-						<li>
-							<NavLink href='/kerio-control'>Kerio Control</NavLink>
-						</li>
-						<li>
-							<NavLink href='/gfi-archiver'>GFI Archiver</NavLink>
-						</li>
-						<li>
-							<NavLink href='/gfi-languard'>GFI LanGuard</NavLink>
-						</li>
-					</ul>
-				</nav>
+		<header className={styles.header}>
+			<div id='headerInnerWrapper'>
+				<div id='headerNavContainer' className={styles.headerNavContainer}>
+					<nav aria-label='Info'>
+						<ul>
+							<li>
+								<NavLink href='/'>Home</NavLink>
+							</li>
+							<li>
+								<NavLink href='/about-us'>About Us</NavLink>
+							</li>
+						</ul>
+					</nav>
+					<nav aria-label='Products'>
+						<ul>
+							<li>
+								<NavLink href='/kerio-connect'>Kerio Connect</NavLink>
+							</li>
+							<li>
+								<NavLink href='/kerio-control'>Kerio Control</NavLink>
+							</li>
+							<li>
+								<NavLink href='/gfi-archiver'>GFI Archiver</NavLink>
+							</li>
+							<li>
+								<NavLink href='/gfi-languard'>GFI LanGuard</NavLink>
+							</li>
+						</ul>
+					</nav>
+				</div>
 			</div>
 		</header>
 	)
