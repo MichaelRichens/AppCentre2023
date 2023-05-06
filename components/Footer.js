810c04b03a2a0eb08d7177cbe9a3d147960f681e
Michael Richens <michael.richens@gmail.com>
2023-05-06 17:11:39 +0100

    Used small tag

--- components/Footer.js
+++ components/Footer.js
@@ -4,13 +4,13 @@ const Footer = () => {
 	return (
 		<footer>
 			<div id='footerInnerWrapper'>
-				<p>
+				<small>
 					© 2006 - {new Date().getFullYear()} AppCentre. AppCentre is a part of{' '}
 					<a href='https://www.macupgrades.co.uk/store/' target='_blank' rel='noopener noreferrer'>
 						MacUpgrades
 					</a>{' '}
 					/ Second Chance PC Ltd. Company number 4331031. Registered for VAT GB 783705210.
-				</p>
+				</small>
 			</div>
 		</footer>
 	)
