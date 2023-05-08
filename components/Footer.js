f40d811a901d53e90738882458febb555aa3f812
Michael Richens <michael.richens@gmail.com>
2023-05-08 12:13:26 +0100

    styling changes

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
+import styles from '../styles/Footer.module.css'
 
 const Footer = () => {
 	return (
-		<footer>
-			<div id='footerInnerWrapper'>
-				<small>
-					© 2006 - {new Date().getFullYear()} AppCentre. AppCentre is a part of{' '}
-					<a href='https://www.macupgrades.co.uk/store/' target='_blank' rel='noopener noreferrer'>
-						MacUpgrades
-					</a>{' '}
-					/ Second Chance PC Ltd. Company number 4331031. Registered for VAT GB 783705210.
-				</small>
-			</div>
-		</footer>
+		<>
+			<div role='none' className={styles.separator}></div>
+			<footer className={styles.footer}>
+				<div id='footerInnerWrapper'>
+					<small>
+						© 2006 - {new Date().getFullYear()} AppCentre. AppCentre is a part of{' '}
+						<Link href='https://www.macupgrades.co.uk/store/' target='_blank' rel='noopener'>
+							MacUpgrades
+						</Link>{' '}
+						/ Second Chance PC Ltd. Company number 4331031. Registered for VAT GB 783705210.
+					</small>
+				</div>
+			</footer>
+		</>
 	)
 }
 
