dc8e6ed9b56f3e877e168e2b3bcac2e2e3399d12
Michael Richens <michael.richens@gmail.com>
2023-05-08 15:46:04 +0100

    completed docblock

--- components/ProductInfoPage.js
+++ components/ProductInfoPage.js
@@ -8,8 +8,10 @@ import styles from '../styles/ProductInfoPage.module.css'
  * @component
  * A component for generating an info page for a product.  It displays the children content alongside a configurator.
  * @param {Object} props - The component props.
- * @param {string} props.productFamily - The product family code.
  * @param {string} props.title - The title to display on the page.
+ * @param {string} props.productFamily - The product family code.
+ * @param {Object[]} props.productData - The product data pulled from the database.
+ * @param {Word} props.unitName - A Word object representing the units the product is sold in.
  * @param {JSX.Element} props.children - The child components to render within the page.
  * @returns {JSX.Element} The ProductInfoPage component.
  */
