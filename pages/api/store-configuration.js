5511c88ea28e93dee72822fe39b0526c9c6343db
Michael Richens <michael.richens@gmail.com>
2023-05-08 17:55:49 +0100

    removed old code

--- pages/api/store-configuration.js
+++ pages/api/store-configuration.js
@@ -1,18 +1,12 @@
-// pages/api/store-configuration.js
-/*
-import { isProductConfiguration } from '../../utils/typeCheck'
-
 export default async function handler(req, res) {
 	if (req.method === 'POST') {
-		const { formData, clientSideConfiguration } = req.body
-
-		if (formData !== null && clientSideConfiguration !== null && isProductConfiguration(clientSideConfiguration)) {
-			res.status(200).json({ message: 'Valid objects received' })
+		const { formData } = req.body
+		if (formData !== null) {
+			res.status(200).json({ message: 'Valid objects received.' })
 		} else {
-			res.status(400).json({ message: 'Invalid objects received' })
+			res.status(400).json({ message: 'formData not received.' })
 		}
 	} else {
-		res.status(405).json({ message: 'Method not allowed' })
+		res.status(405).json({ message: 'Method not allowed.' })
 	}
 }
-*/
