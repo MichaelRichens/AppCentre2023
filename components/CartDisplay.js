564af24a754cdd7bb823b5b59201d54985c14617
Michael Richens <michael.richens@gmail.com>
2023-05-12 22:28:01 +0100

    worked on cart

--- components/CartDisplay.js
+++ components/CartDisplay.js
@@ -1,37 +1,41 @@
 import React from 'react'
 import { useShoppingCart } from 'use-shopping-cart'
+import { Tooltip } from 'react-tooltip'
 import styles from '../styles/CartDisplay.module.css'
 
 const CartDisplay = () => {
-	const { cartDetails, clearCart, formattedTotalPrice, removeItem } = useShoppingCart()
-
-	const handleClearCart = () => {
-		clearCart()
-	}
+	const { cartDetails, formattedTotalPrice, removeItem } = useShoppingCart()
 
 	const handleRemoveItem = (id) => {
 		removeItem(id)
 	}
 
 	return (
-		<div className={styles.cartContainer}>
+		<form className={styles.cartContainer}>
 			<h2>Your Cart</h2>
-			<ul>
-				{Object.keys(cartDetails).map((itemID) => {
-					const item = cartDetails[itemID]
-					return (
-						<li key={itemID}>
-							<button onClick={() => handleRemoveItem(itemID)} aria-label='Remove Item'>
-								X
-							</button>
-							{` ${item.quantity} x ${item.name}`}
-						</li>
-					)
-				})}
-			</ul>
+			<fieldset className={styles.cartItems}>
+				<legend>Items</legend>
+				<ul>
+					{Object.keys(cartDetails).map((itemID) => {
+						const item = cartDetails[itemID]
+						return (
+							<li key={itemID}>
+								<button
+									onClick={() => handleRemoveItem(itemID)}
+									aria-label='Delete Item'
+									data-tooltip-id={`remove-item-${itemID}`}
+									data-tooltip-content='Delete Item'>
+									X
+								</button>
+								<Tooltip id={`remove-item-${itemID}`} />
+								{` ${item.quantity > 1 ? item.quantity + ' x ' : ''}${item.name}`}
+							</li>
+						)
+					})}
+				</ul>
+			</fieldset>
 			<h3>Total: {formattedTotalPrice}</h3>
-			<button onClick={handleClearCart}>Clear Cart</button>
-		</div>
+		</form>
 	)
 }
 
