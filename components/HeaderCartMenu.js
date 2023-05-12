57c05f8edd319698f01e92d52574d4210bfc4265
Michael Richens <michael@2ndchancepc.co.uk>
2023-05-12 11:03:49 +0100

    basic click to open cart implemented

--- components/HeaderCartMenu.js
+++ components/HeaderCartMenu.js
@@ -1,19 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import { useShoppingCart } from 'use-shopping-cart'
+import CartDisplay from './CartDisplay'
 import headerStyles from '../styles/Header.shared.module.css'
 
 const HeaderCartMenu = () => {
 	const { cartCount } = useShoppingCart()
+	const [isCartVisible, setCartVisible] = useState(false)
+
+	const handleCartClick = () => {
+		if (cartCount > 0) {
+			setCartVisible(!isCartVisible)
+		}
+	}
+
+	const handleCartClose = () => {
+		setCartVisible(false)
+	}
 
 	return (
 		<aside id='headerCartContainer' className={headerStyles.headerCartContainer}>
-			<div className={headerStyles.cartIcon}>
-				<Image src='/images/icons/shopping_cart_icon100x100.png' height='30' width='30' alt='' />
-				<div className={`${headerStyles.cartCount} ${cartCount > 0 ? headerStyles.cartFull : headerStyles.cartEmpty}`}>
+			<button
+				className={headerStyles.cartIcon}
+				onClick={handleCartClick}
+				aria-label='Open cart'
+				title='Click to Open'
+				disabled={cartCount === 0}>
+				<Image src='/images/icons/shopping_cart_icon100x100.png' height='30' width='30' alt='Shopping Cart' />
+				<div
+					aria-label='Items in Cart'
+					className={`${headerStyles.cartCount} ${cartCount > 0 ? headerStyles.cartFull : headerStyles.cartEmpty}`}>
 					{cartCount}
 				</div>
-			</div>
+			</button>
+			{isCartVisible && (
+				<div className={headerStyles.cartWrapper}>
+					<button onClick={handleCartClose} className={headerStyles.closeButton} aria-label='Close cart'>
+						X
+					</button>
+					<div className={headerStyles.cart}>
+						<CartDisplay />
+					</div>
+				</div>
+			)}
 		</aside>
 	)
 }
