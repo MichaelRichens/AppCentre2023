d38bdc8c651198c257f13a8d3ea7e9b3e8014fa7
Michael Richens <michael@2ndchancepc.co.uk>
2023-05-12 10:10:52 +0100

    somewhat functional popup cart menu

--- components/HeaderCartMenu.js
+++ components/HeaderCartMenu.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import Image from 'next/image'
 import { useShoppingCart } from 'use-shopping-cart'
+import CartDisplay from './CartDisplay'
 import headerStyles from '../styles/Header.shared.module.css'
 
 const HeaderCartMenu = () => {
 	const { cartCount } = useShoppingCart()
+	const [isCartVisible, setCartVisible] = useState(false)
+	const leaveTimeout = useRef(null)
+
+	const handleMouseEnter = () => {
+		if (leaveTimeout.current) {
+			clearTimeout(leaveTimeout.current)
+		}
+		setCartVisible(true)
+	}
+
+	const handleMouseLeave = () => {
+		leaveTimeout.current = setTimeout(() => {
+			setCartVisible(false)
+		}, 500) // adjust the delay time as needed
+	}
 
 	return (
 		<aside id='headerCartContainer' className={headerStyles.headerCartContainer}>
-			<div className={headerStyles.cartIcon}>
-				<Image src='/images/icons/shopping_cart_icon100x100.png' height='30' width='30' alt='' />
-				<div className={`${headerStyles.cartCount} ${cartCount > 0 ? headerStyles.cartFull : headerStyles.cartEmpty}`}>
+			<div className={headerStyles.cartIcon} onMouseEnter={handleMouseEnter}>
+				<Image src='/images/icons/shopping_cart_icon100x100.png' height='30' width='30' alt='Shopping Cart' />
+				<div
+					aria-label='Items in Cart'
+					className={`${headerStyles.cartCount} ${cartCount > 0 ? headerStyles.cartFull : headerStyles.cartEmpty}`}>
 					{cartCount}
+				</div>
+			</div>
+			<div className={headerStyles.cartWrapper} onMouseLeave={handleMouseLeave}>
+				<div className={`${headerStyles.cart} ${isCartVisible ? headerStyles.open : ''}`}>
+					<CartDisplay />
 				</div>
 			</div>
 		</aside>
