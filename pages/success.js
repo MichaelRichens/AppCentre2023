9216a7352afb08fcda0133fe8768a330033215e9
Michael Richens <michael.richens@gmail.com>
2023-05-15 20:25:09 +0100

    adding addional hacky clear cart logic to try and get it working on android

--- pages/success.js
+++ pages/success.js
@@ -11,17 +11,29 @@ const OrderSuccess = () => {
 		const urlParams = new URLSearchParams(window.location.search)
 		const urlSessionId = urlParams.get('session_id')
 		const sessionStorageSessionId = sessionStorage.getItem('checkoutSessionId')
-
 		let timeoutId
+
+		const hackyClearCart = () => {
+			localStorage.setItem(process.env.NEXT_PUBLIC_CART_PERSIST_KEY, JSON.stringify({}))
+		}
+
 		if (urlSessionId && sessionStorageSessionId && urlSessionId === sessionStorageSessionId) {
 			//  HACKY WORKAROUND
 			// clearCart isn't working on page load for some unknown reason, so removing cart from localStorage directly
 			// suspect this may be related to use-shopping-cart wanting React 17, but having React 18 installed
-			localStorage.setItem(process.env.NEXT_PUBLIC_CART_PERSIST_KEY, JSON.stringify({}))
+			hackyClearCart()
+
+			timeoutId = setTimeout(() => {
+				hackyClearCart()
+			}, 500)
+
 			sessionStorage.removeItem('checkoutSessionId')
 		}
 		return () => {
-			sessionStorage.removeItem('checkoutSessionId')
+			if (urlSessionId && sessionStorageSessionId && urlSessionId === sessionStorageSessionId) {
+				hackyClearCart()
+				sessionStorage.removeItem('checkoutSessionId')
+			}
 			clearTimeout(timeoutId)
 		}
 	}, [])
