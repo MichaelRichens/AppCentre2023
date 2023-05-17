f27d976bfff530de7dd303101e0fe89f0eb48886
Michael Richens <michael.richens@gmail.com>
2023-05-17 19:04:31 +0100

    Proxy wrappers for enums in dev mode to throw errors on invalid property usage

--- utils/types/enums/PricingType.js
+++ utils/types/enums/PricingType.js
@@ -1,12 +1,28 @@
-/**
- * PricingType Enum.
- *
- * @enum {string}
- * @readonly
- * @property {string} UNIT - Pricing is done on a per unit (eg user) basis
- */
-const PricingType = Object.freeze({
-	UNIT: 'unit',
-})
+// Define the base Enum-like object for PricingType.
+// This is a simple JavaScript object that maps your pricing types to their string representations.
+const PricingTypeEnum = {
+	UNIT: 'unit', // Pricing is done on a per unit (eg user) basis
+}
 
+// Depending on the environment, we define PricingType in different ways.
+// In development, we want to use a Proxy to wrap PricingTypeEnum, which will throw an error if we try to access a property that doesn't exist.
+// In production, we want to avoid the overhead of the Proxy, so we use the base PricingTypeEnum object directly.
+const PricingType =
+	process.env.NODE_ENV === 'development'
+		? new Proxy(PricingTypeEnum, {
+				// The `get` method is called whenever a property on PricingType is accessed.
+				// If the property exists on the PricingTypeEnum object, its value is returned.
+				// If the property doesn't exist, we throw an error.
+				get(target, name) {
+					if (name in target) {
+						return target[name]
+					} else {
+						throw new Error(`Invalid PricingType: ${name}`)
+					}
+				},
+		  })
+		: // In production mode, we simply use the base Enum-like object.
+		  PricingTypeEnum
+
+// Export the PricingType object for use in other modules.
 export default PricingType
--- utils/types/enums/PurchaseType.js
+++ utils/types/enums/PurchaseType.js
@@ -1,18 +1,31 @@
-/**
- * PurchaseType Enum.
- *
- * @enum {string}
- * @readonly
- * @property {string} SUB - An extension of an existing subscription, which may have other options like units/users or extensions modified.
- * @property {string} NEW - A new subscription.
- * @property {string} ADD - Additional units/users to an existing subscription.
- * @property {string} EXT - Additional extension(s) to an existing subscription.
- */
-const PurchaseType = Object.freeze({
-	SUB: 'sub',
-	NEW: 'new',
-	ADD: 'add',
-	EXT: 'ext',
-})
+// Define the base Enum-like object.
+// This is a simple JavaScript object that maps your purchase types to their string representations.
+const PurchaseTypeEnum = {
+	SUB: 'sub', // An extension of an existing subscription
+	NEW: 'new', // A new subscription
+	ADD: 'add', // Additional units/users to an existing subscription
+	EXT: 'ext', // Additional extension(s) to an existing subscription
+}
 
+// Depending on the environment, we define PurchaseType in different ways.
+// In development, we want to use a Proxy to wrap PurchaseTypeEnum, which will throw an error if we try to access a property that doesn't exist.
+// In production, we want to avoid the overhead of the Proxy, so we use the base PurchaseTypeEnum object directly.
+const PurchaseType =
+	process.env.NODE_ENV === 'development'
+		? new Proxy(PurchaseTypeEnum, {
+				// The `get` method is called whenever a property on PurchaseType is accessed.
+				// If the property exists on the PurchaseTypeEnum object, its value is returned.
+				// If the property doesn't exist, we throw an error.
+				get(target, name) {
+					if (name in target) {
+						return target[name]
+					} else {
+						throw new Error(`Invalid PurchaseType: ${name}`)
+					}
+				},
+		  })
+		: // In production mode, we simply use the base Enum-like object.
+		  PurchaseTypeEnum
+
+// Export the PurchaseType object for use in other modules.
 export default PurchaseType
