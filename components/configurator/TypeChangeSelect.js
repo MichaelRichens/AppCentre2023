b3e93e7f2b0c40faf5c7d70e09df2d87f5ce0939
Michael Richens <michael@2ndchancepc.co.uk>
2023-05-18 11:22:28 +0100

    removed outdated documentation

--- components/configurator/TypeChangeSelect.js
+++ components/configurator/TypeChangeSelect.js
@@ -2,18 +2,11 @@ import React from 'react'
 import PurchaseType from '../../utils/types/enums/PurchaseType'
 
 /**
- * TypeChangeSelect component allows the user to select a purchase type.
- *
- * The component renders a dropdown menu with options to choose between
- * different types of configurator actions, such as subscription renewal, new subscription purchase, etc.
- * The PurchaseType.ADD option is conditionally rendered based on the showaddUserOption prop.
- *
  * @component
+ * TypeChangeSelect component allows the user to select a purchase type.
  * @param {Object} props - The component's props.
  * @param {PurchaseType} props.type - The current selected type of configurator action.
  * @param {Object} props.typeOptions - The available options to choose from, has members of the PurchaseType enum as keys, with values being the text to display for them
- * @param {string|boolean} props.addUserOption - Pass false to not show the PurchaseType.ADD option as a choice, otherwise pass the text to display for that option.
- * @param {string|boolean} props.addExtOption - Pass false to not show the PurchaseType.EXT option as a choice, otherwise pass the text to display for that option.
  * @param {function} props.onTypeChange - Callback to handle changes to the selected type.
  * @returns {ReactElement} The rendered TypeChangeSelect component.
  */
