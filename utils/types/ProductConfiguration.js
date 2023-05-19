10fe554da6a922ed007a36ae7e889112f7eb5fc3
Michael Richens <michael.richens@gmail.com>
2023-05-19 07:27:14 +0100

    Reordered param documentation

--- utils/types/ProductConfiguration.js
+++ utils/types/ProductConfiguration.js
@@ -9,9 +9,9 @@
 
 class ProductConfiguration {
 	/**
-	 * @param {number} years - The number of years the subscription will last with this configuration, may be fractional, which is allowed for some type values.
 	 * @param {string} type - The type of the configuration - 'new', 'sub' ect.
 	 * @param {number} units - The number of units (eg users).
+	 * @param {number} years - The number of years the subscription will last with this configuration, may be fractional, which is allowed for some type values.
 	 * @param {number} [price=0] - The price of the selected product configuration.
 	 * @param {Object} [skus={}] - An object containing the SKUs for the product as keys, and quantities as values.
 	 * @param {ConfigurationSummary|null} [summary=null] - The summary of the product, object that contains text information intended for user display.  May be null if it isn't generated yet.
