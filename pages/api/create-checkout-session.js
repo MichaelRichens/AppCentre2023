0679e7df94585ea80606ddb967078409f6558ebb
Michael Richens <michael@2ndchancepc.co.uk>
2023-05-22 13:34:42 +0100

    renamed variables

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -11,9 +11,9 @@ export default async (req, res) => {
 				return res.status(400).json({ message: "Invalid request body. Expected 'items' array." })
 			}
 
-			let trustedProductData
+			let trustedConfigurations
 			try {
-				trustedProductData = Object.fromEntries(
+				trustedConfigurations = Object.fromEntries(
 					await Promise.all(
 						cartFromClientSide.items.map(async (item) => [item.id, await asyncGetConfiguration(item.id)])
 					)
@@ -27,29 +27,31 @@ export default async (req, res) => {
 
 			let isShipping = false
 
-			for (let key in trustedProductData) {
-				if (trustedProductData[key].hasOwnProperty('isShipping') && trustedProductData[key].isShipping === true) {
+			for (let key in trustedConfigurations) {
+				if (trustedConfigurations[key].hasOwnProperty('isShipping') && trustedConfigurations[key].isShipping === true) {
 					isShipping = true
 					break
 				}
 			}
-			// create shipping properties for session object
-			let shipping_address_collection = null
-			if (isShipping) {
-				shipping_address_collection = { allowed_countries: process.env.NEXT_PUBLIC_SHIPPING_COUNTRIES.split(',') }
-			}
 
 			// Create an array of line items for the Stripe checkout session
-			const line_items = Object.keys(trustedProductData).map((id) => {
-				const item = trustedProductData[id]
-				const itemName = item.description
+			const line_items = Object.keys(trustedConfigurations).map((id) => {
+				const configuration = trustedConfigurations[id]
+				const itemName = configuration.description
 
-				if (!item || !item.summary || !item.summary || !item.price || !itemName || typeof item.price !== 'number') {
+				if (
+					!configuration ||
+					!configuration.summary ||
+					!configuration.summary ||
+					!configuration.price ||
+					!itemName ||
+					typeof configuration.price !== 'number'
+				) {
 					console.error(`Invalid product data for ID ${id}`)
 					throw new Error(`Invalid product data for ID ${id}`)
 				}
 
-				const priceInPence = Math.round(item.price * 100)
+				const priceInPence = Math.round(configuration.price * 100)
 
 				// untrusted data from the client, just using it for the cart quantity
 				const cartItem = cartFromClientSide.items.find((item) => item.id === id)
@@ -60,10 +62,10 @@ export default async (req, res) => {
 
 				const metadata = {
 					internalId: id,
-					configuration: item.unType,
-					units: item.units,
-					years: item.years,
-					skus: JSON.stringify(item.skus),
+					configuration: configuration.unType,
+					units: configuration.units,
+					years: configuration.years,
+					skus: JSON.stringify(configuration.skus),
 				}
 
 				return {
@@ -100,7 +102,9 @@ export default async (req, res) => {
 			}
 
 			if (isShipping) {
-				sessionCreationObj['shipping_address_collection'] = shipping_address_collection
+				sessionCreationObj['shipping_address_collection'] = {
+					allowed_countries: process.env.NEXT_PUBLIC_SHIPPING_COUNTRIES.split(','),
+				}
 			}
 
 			let session
