90b681e1abde9050ed9a51c5c112990a7a9c7d00
Michael Richens <michael@2ndchancepc.co.uk>
2023-05-24 11:48:32 +0100

    removed old code

--- pages/success.js
+++ pages/success.js
@@ -3,45 +3,25 @@ import { CartContext } from '../components/contexts/CartContext'
 import Page from '../components/Page'
 
 const OrderSuccess = () => {
-	const { clearCart, cartDetails, removeItem } = useShoppingCart()
+	const { clearCart } = useContext(CartContext)
 	const [sessionIdState, setSessionIdState] = useState(null)
 
 	useEffect(() => {
 		const urlParams = new URLSearchParams(window.location.search)
 		const urlSessionId = urlParams.get('session_id')
 		const sessionStorageSessionId = sessionStorage.getItem('checkoutSessionId')
-		// tmp for testing api - use the commented out call inside the first load check
-		setSessionIdState(urlSessionId)
-		let timeoutId
-
-		const hackyClearCart = () => {
-			localStorage.removeItem(process.env.NEXT_PUBLIC_CART_PERSIST_KEY)
-		}
 
 		if (urlSessionId && sessionStorageSessionId && urlSessionId === sessionStorageSessionId) {
-			//this is where the state should be saved, though not 100% sure sessionStorageSessionId is the right source, but it probably is
-			//setSessionIdState(sessionStorageSessionId)
-
-			//  HACKY WORKAROUND
-			// clearCart isn't working on page load for some unknown reason, so removing cart from localStorage directly
-			// suspect this may be related to use-shopping-cart wanting React 17, but having React 18 installed
-			hackyClearCart()
-			// However testing on Chrome on Android on my phone, the above doesn't work.  So...
-			// Do it again after a timeout...
-			// When this happens, it doesn't trigger a rerender of the cart component, so we've also have to hide the cart entirely on the success page
-			timeoutId = setTimeout(() => {
-				hackyClearCart()
-			}, 500)
-
+			setSessionIdState(urlSessionId)
 			sessionStorage.removeItem('checkoutSessionId')
+			clearCart()
 		}
+
 		return () => {
 			if (urlSessionId && sessionStorageSessionId && urlSessionId === sessionStorageSessionId) {
 				sessionStorage.removeItem('checkoutSessionId')
-				// Just to be on the safe side, we'll clear the bloody cart here as well.
-				hackyClearCart()
+				clearCart()
 			}
-			clearTimeout(timeoutId)
 		}
 	}, [])
 
