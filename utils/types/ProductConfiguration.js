22339c41e138b7814b8571ff72103767af3b2928
Michael Richens <michael.richens@gmail.com>
2023-05-24 22:07:20 +0100

    Added licence to ProductConfiguration.

--- utils/types/ProductConfiguration.js
+++ utils/types/ProductConfiguration.js
@@ -18,21 +18,23 @@ export class ProductConfiguration {
 	 * @param {Object} [skus={}] - An object containing the SKUs for the product as keys, and quantities as values.
 	 * @param {ConfigurationSummaryUnit|null} [summary=null] - The summary of the product, object that contains text information intended for user display.  Its class depends on the pricingType.  May be null if it isn't generated yet.
 	 * @param {boolean} [isShipping=false] - A flag that can be set to true if something on this order requires being shipped.
+	 * @param {string} [licence=''] - Optional. The licence code of a subscription being modified.  At time of writing only ever present on instances pulled from the database and created via fromRawProperties (since licence gets added directly to the database from the cart, this object is unused once the cart has been populated).
 	 * @static fromRawProperties - Creates a new instance from the properties of the passed object, assumed to be a raw serialisation of another instance (ie will use backing field names not getter names)
 	 */
-	constructor(pricingType, type, price = 0, skus = {}, summary = null, isShipping = false) {
+	constructor(pricingType, type, price = 0, skus = {}, summary = null, isShipping = false, licence = '') {
 		this.pricingType = pricingType
 		this.type = type
 		this._price = parseFloat(price.toFixed(2)) // Store the price in a private variable
 		this.skus = skus
 		this.summary = summary
 		this.isShipping = isShipping
+		this.licence = licence
 	}
 
 	static fromRawProperties(properties) {
-		const { pricingType, type, _price, skus, summary, isShipping } = properties
+		const { pricingType, type, _price, skus, summary, isShipping, licence } = properties
 
-		return new ProductConfiguration(pricingType, type, _price, skus, summary, isShipping)
+		return new ProductConfiguration(pricingType, type, _price, skus, summary, isShipping, licence)
 	}
 
 	get price() {
@@ -49,9 +51,11 @@ export class ProductConfiguration {
 		}
 		switch (this.pricingType) {
 			case PricingType.UNIT:
-				return `${this.summary.product}${this.summary.extensions ? ' ' + this.summary.extensions : ''}`
+				return `${this.licence.length > 0 ? this.licence + ' :' : ''}${this.summary.product}${
+					this.summary.extensions ? ' ' + this.summary.extensions : ''
+				}`
 			default:
-				return this.summary.product
+				return (this.licence.length > 0 ? this.licence + ' :' : '') + this.summary.product
 		}
 	}
 }
