bc8062f6c41d152c7107a3dafc787356ecbee289
Michael Richens <michael.richens@gmail.com>
2023-05-24 21:31:50 +0100

    changed update lice api to permit removal of licence

--- pages/api/update-configuration-licence.js
+++ pages/api/update-configuration-licence.js
@@ -7,12 +7,14 @@ export default async function handler(req, res) {
 		return res.status(400).json({ error: 'Invalid or missing id.' })
 	}
 
-	if (!licence || typeof licence !== 'string' || licence.length === 0) {
-		return res.status(400).json({ error: 'Invalid or missing licence.' })
+	if (!('licence' in req.body)) {
+		return res.status(400).json({ error: 'Licence must be provided.' })
 	}
 
+	const licenceToUpdate = licence === null || licence === '' || typeof licence !== 'string' ? undefined : licence
+
 	try {
-		await asyncUpdateRecord('configurations', id, { licence })
+		await asyncUpdateRecord('configurations', id, { licence: licenceToUpdate })
 		return res.status(200).json({ message: 'Configuration licence updated successfully.' })
 	} catch (error) {
 		console.error('Error updating configuration licence:', error)
