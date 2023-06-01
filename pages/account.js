57e049426fe4c74b367b1742d0fd95750e3dad41
Michael Richens <michael@2ndchancepc.co.uk>
2023-06-01 10:43:24 +0100

    Basic address display component added

--- pages/account.js
+++ pages/account.js
@@ -3,6 +3,7 @@ import Page from '../components/page/Page'
 import withAuth from '../components/hoc/withAuth'
 import { useAuth } from '../components/contexts/AuthContext'
 import CustomerOrders from '../components/account/CustomerOrders'
+import Address from '../components/account/Address'
 import { firestore } from '../utils/firebaseClient'
 import { doc, onSnapshot } from 'firebase/firestore'
 import accountStyles from '../styles/Account.shared.module.css'
@@ -11,6 +12,10 @@ const Account = () => {
 	const { user } = useAuth()
 	const [userDetails, setUserDetails] = useState({})
 	const [orders, setOrders] = useState([])
+
+	const showAddress = userDetails?.billingAddress
+	const showShippingAddress =
+		userDetails?.shippingAddress && userDetails?.shippingAddress?.path !== userDetails?.billingAddress?.path
 
 	useEffect(() => {
 		const userDocRef = doc(firestore, 'users', user.uid)
@@ -48,6 +53,20 @@ const Account = () => {
 				<h2>Orders</h2>
 				<CustomerOrders user={user} />
 			</section>
+			{showAddress && (
+				<section className={accountStyles.address}>
+					<div>
+						<h2>{showShippingAddress ? 'Billing Address' : 'Address'}</h2>
+						<Address addressRef={userDetails.billingAddress} />
+					</div>
+					{showShippingAddress && (
+						<div>
+							<h2>Shipping Address</h2>
+							<Address addressRef={userDetails.shippingAddress} />
+						</div>
+					)}
+				</section>
+			)}
 		</Page>
 	)
 }
