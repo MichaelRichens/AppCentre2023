f6df370018b58fce2dda1a5ae57d0ba3648e0c28
Michael Richens <michael@2ndchancepc.co.uk>
2023-06-02 11:20:27 +0100

    put contact form above contact details

--- pages/contact.js
+++ pages/contact.js
@@ -7,35 +7,38 @@ import contactStyles from '../styles/Contact.shared.module.css'
 const Contact = () => {
 	return (
 		<Page title='Contact Us'>
-			<section className={contactStyles.contactDetails}>
-				<div className={contactStyles.text}>
-					<p>
-						You can contact us with any queries you have using the email form below, or call us during our office hours
-						- Monday - Friday, 9am - 5pm.
-					</p>
-				</div>
-				<ul className={contactStyles.details}>
-					<li>
-						<strong>Telephone:</strong>
-						<p>01223 833 412</p>
-					</li>
-					<li>
-						<strong>Email:</strong>
-						<p>info@appcentre.co.uk</p>
-					</li>
-					<li>
-						<strong>Address:</strong>
-						<p>Unit 5</p>
-						<p>South Cambridgeshire Business Park</p>
-						<p>Sawston</p>
-						<p>Cambridge CB22 3JH</p>
-						<p>United Kingdom</p>
-					</li>
-				</ul>
-			</section>
 			<section>
 				<h2>Email Us</h2>
 				<ContactForm />
+			</section>
+			<section>
+				<h2>Contact Details</h2>
+				<div className={contactStyles.contactDetails}>
+					<div className={contactStyles.text}>
+						<p>
+							You can contact us with any queries you have using the email form above, or call us during our office
+							hours - Monday - Friday, 9am - 5pm.
+						</p>
+					</div>
+					<ul className={contactStyles.details}>
+						<li>
+							<strong>Telephone:</strong>
+							<p>01223 833 412</p>
+						</li>
+						<li>
+							<strong>Email:</strong>
+							<p>info@appcentre.co.uk</p>
+						</li>
+						<li>
+							<strong>Address:</strong>
+							<p>Unit 5</p>
+							<p>South Cambridgeshire Business Park</p>
+							<p>Sawston</p>
+							<p>Cambridge CB22 3JH</p>
+							<p>United Kingdom</p>
+						</li>
+					</ul>
+				</div>
 			</section>
 		</Page>
 	)
