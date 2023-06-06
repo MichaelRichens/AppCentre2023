5731bb2d59045e61c10e114ba35571ab1dbf8fdc
Michael Richens <michael.richens@gmail.com>
2023-06-06 20:49:18 +0100

    Fiddled with layout

--- pages/contact.js
+++ pages/contact.js
@@ -7,39 +7,41 @@ import contactStyles from '/styles/Contact.shared.module.css'
 const Contact = () => {
 	return (
 		<Page title='Contact Us'>
-			<section>
-				<h2>Email Us</h2>
-				<ContactForm />
-			</section>
-			<section>
-				<h2>Contact Details</h2>
-				<div className={contactStyles.contactDetails}>
-					<div className={contactStyles.text}>
-						<p>
-							You can contact us with any queries you have using the email form above, or call us during our office
-							hours - Monday - Friday, 9am - 5pm.
-						</p>
+			<div className={contactStyles.contactFormAndDetails}>
+				<section>
+					<h2>Email Us</h2>
+					<ContactForm />
+				</section>
+				<section>
+					<h2>Contact Details</h2>
+					<div className={contactStyles.contactDetails}>
+						<div className={contactStyles.text}>
+							<p>
+								You can contact us with any queries you have using the email form, or call us during our office hours -
+								Monday - Friday, 9am - 5pm.
+							</p>
+						</div>
+						<ul className={contactStyles.details}>
+							<li>
+								<strong>Telephone:</strong>
+								<p>01223 833 412</p>
+							</li>
+							<li>
+								<strong>Email:</strong>
+								<p>info@appcentre.co.uk</p>
+							</li>
+							<li>
+								<strong>Address:</strong>
+								<p>Unit 5</p>
+								<p>South Cambridgeshire Business Park</p>
+								<p>Sawston</p>
+								<p>Cambridge CB22 3JH</p>
+								<p>United Kingdom</p>
+							</li>
+						</ul>
 					</div>
-					<ul className={contactStyles.details}>
-						<li>
-							<strong>Telephone:</strong>
-							<p>01223 833 412</p>
-						</li>
-						<li>
-							<strong>Email:</strong>
-							<p>info@appcentre.co.uk</p>
-						</li>
-						<li>
-							<strong>Address:</strong>
-							<p>Unit 5</p>
-							<p>South Cambridgeshire Business Park</p>
-							<p>Sawston</p>
-							<p>Cambridge CB22 3JH</p>
-							<p>United Kingdom</p>
-						</li>
-					</ul>
-				</div>
-			</section>
+				</section>
+			</div>
 		</Page>
 	)
 }
