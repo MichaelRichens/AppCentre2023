56e6ab70c2d05ed1ce4d5616799c3fa53fb71829
Michael Richens <michael.richens@gmail.com>
2023-06-11 16:42:17 +0100

    Did proper footer

--- components/page/Footer.js
+++ components/page/Footer.js
@@ -1,21 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 import styles from '/styles/Footer.module.css'
 
 const Footer = () => {
+	const router = useRouter()
+
 	return (
 		<>
 			<div role='none' className={styles.separator}></div>
 			<footer className={styles.footer}>
-				<div id='footerInnerWrapper'>
-					<small>
-						© 2006 - {new Date().getFullYear()} AppCentre. AppCentre is a part of{' '}
-						<Link href='https://www.macupgrades.co.uk/store/' target='_blank' rel='noopener'>
-							MacUpgrades
-						</Link>{' '}
-						/ Second Chance PC Ltd. Company Number: {process.env.NEXT_PUBLIC_COMPANY_NUMBER}. Registered for VAT:{' '}
-						{process.env.NEXT_PUBLIC_VAT_NUMBER}. See our <Link href='/privacy'>privacy policy</Link>.
-					</small>
+				<div id='footerInnerWrapper' className='styles.footerInnerWrapper'>
+					<div className={styles.contactDetails}>
+						<h2>Contact Details</h2>
+						<div className={styles.contactDetailsContent}>
+							<div className={styles.phoneEmail}>
+								<div>
+									<strong>Tel:</strong> 01223 833 412
+								</div>
+								<div>
+									<strong>Email:</strong>{' '}
+									{router?.pathname !== '/contact' ? (
+										<Link href='/contact'>info@appcentre.co.uk</Link>
+									) : (
+										'info@appcentre.co.uk'
+									)}
+								</div>
+							</div>
+							<div className={styles.address}>
+								<strong>Address:</strong>
+								<ul>
+									<li>Unit 5 South Cambridgeshire Business Park</li>
+									<li>Sawston</li>
+									<li>Cambridge</li>
+									<li>CB22 3JH</li>
+									<li>United Kingdom</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div className={styles.smallPrintWrapper}>
+						<small>
+							© 2006 - {new Date().getFullYear()} AppCentre. AppCentre is a part of{' '}
+							<Link href='https://www.macupgrades.co.uk/store/' target='_blank' rel='noopener'>
+								MacUpgrades
+							</Link>{' '}
+							/ Second Chance PC Ltd. Company Number: {process.env.NEXT_PUBLIC_COMPANY_NUMBER}. Registered for VAT:{' '}
+							{process.env.NEXT_PUBLIC_VAT_NUMBER}. See our <Link href='/privacy'>privacy policy</Link>.
+						</small>
+					</div>
 				</div>
 			</footer>
 		</>
