48cdefe1e3ef7e2f815fdb22162fa22230e64013
Michael Richens <michael.richens@gmail.com>
2023-06-11 16:49:30 +0100

    text tweaks

--- pages/ordering.js
+++ pages/ordering.js
@@ -9,12 +9,11 @@ const Ordering = () => {
 				<section>
 					<p>
 						We aim to make the process of pricing up and ordering GFI and Kerio products as quick and painless as
-						possible. To facilitate this, we have tools on all the product pages on the site which allow you to
-						configure a new subscription or a modification to an existing one. You can use one of these to select all
-						the details you need for your installation, and it will pick the correct tier pricing for your subscription,
-						and give you a total price at the bottom - allowing you to make quick comparisons of different options. When
-						you have made a decision, you can add the custom configuration straight to the cart and check it out as an
-						order.
+						possible. To facilitate this, we have tools on all the product pages which allow you to configure a new
+						subscription or a modification to an existing one. You can use one of these to select all the details you
+						need for your installation, and it will pick the correct tier pricing for your subscription, and give you a
+						total price at the bottom - allowing you to make quick comparisons of different options. When you have made
+						a decision, you can add the custom configuration straight to the cart and check it out as an order.
 					</p>
 					<p>
 						From the shopping cart, you can also create a link to save/share the configuration(s) currently in your
@@ -23,9 +22,9 @@ const Ordering = () => {
 						quote.
 					</p>
 					<p>
-						For renewals and modifications you can also, in the shopping cart, enter your licence key to link it with
-						the configuration - this will help us find your subscription to process the update. If you are not an
-						existing customer, we may need to contact you for this information if you do not provide it.
+						For renewals and modifications you can also in the shopping cart, enter your licence key to link it with the
+						configuration - this will help us find your subscription to process the update. If you are not an existing
+						customer, we may need to contact you for this information if you do not provide it.
 					</p>
 					<p>Payment is via credit/debit card with Visa, Mastercard, American Express and Discover accepted.</p>
 					<p>
