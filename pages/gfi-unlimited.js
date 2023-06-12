faca34a34d783f57524f4e3c91e83251a1b23d6c
Michael Richens <michael@2ndchancepc.co.uk>
2023-06-12 13:01:54 +0100

    Basic text for gfi unlimited page

--- pages/gfi-unlimited.js
+++ pages/gfi-unlimited.js
@@ -19,7 +19,18 @@ export async function getStaticProps() {
 const UnlimitedLegacy = (props) => {
 	const { productDataArray } = props
 	console.log(productDataArray)
-	return <PricingPage productIntro={<p>GFI Unlimited...</p>} productDataArray={productDataArray}></PricingPage>
+	return (
+		<PricingPage
+			subHeading='Legacy Product (Renewals Only)'
+			productIntro={
+				<p>
+					GFI Unlimited was a software bundle offered by GFI that allowed access to a wide range of their software with
+					a single subscription. This bundle is no longer a current product, and while existing customers can renew
+					their current users, additional users cannot be added, and new subscriptions are not available.
+				</p>
+			}
+			productDataArray={productDataArray}></PricingPage>
+	)
 }
 
 export default UnlimitedLegacy
