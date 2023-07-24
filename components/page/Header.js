5057f09f817970ff45aa8bea799a7579034ec1e4
Michael Richens <michael.richens@gmail.com>
2023-07-24 11:10:25 +0100

    Added banner message that the site is no longer trading.

--- components/page/Header.js
+++ components/page/Header.js
@@ -1,4 +1,5 @@
 import React, { useContext, useRef } from 'react'
+import Link from 'next/link'
 import useIsAtLeastTwiceChildHeight from '../hooks/useIsAtLeastTwiceChildHeight'
 import FlashMessage from '../FlashMessage'
 import HeaderCartMenu from './HeaderCartMenu'
@@ -20,136 +21,149 @@ const Header = () => {
 	const { user, isAuthLoading } = useAuth()
 
 	return (
-		<header className={headerStyles.header}>
-			<FlashMessage />
-			<div id='headerInnerWrapper' className={headerStyles.headerInnerWrapper}>
-				<div className={headerStyles.headerMainContent}>
-					<div
-						ref={topLinksRef}
-						className={`${headerStyles.topLinks} ${topLinksIsMultiRow ? headerStyles.multiRowTopLinks : ''}`}>
-						<nav className={headerStyles.infoMenu} aria-label='Info'>
-							<ul>
+		<>
+			<aside className={headerStyles.textNotice}>
+				<p>
+					We regret to announce that AppCentre / Second Chance PC Ltd is no longer trading, and orders placed on this
+					site will not be processed.
+				</p>
+				<p>
+					Many thanks for your business over the years. To renew your GFI products you can contact Infinigate, GFI's UK
+					distributor, at <Link href='mailto:gfi@infinigate.co.uk'>gfi@infinigate.co.uk</Link>, and they will put you in
+					touch with a reseller who can help you.
+				</p>
+			</aside>
+			<header className={headerStyles.header}>
+				<FlashMessage />
+				<div id='headerInnerWrapper' className={headerStyles.headerInnerWrapper}>
+					<div className={headerStyles.headerMainContent}>
+						<div
+							ref={topLinksRef}
+							className={`${headerStyles.topLinks} ${topLinksIsMultiRow ? headerStyles.multiRowTopLinks : ''}`}>
+							<nav className={headerStyles.infoMenu} aria-label='Info'>
+								<ul>
+									<li>
+										<NavLink href='/' currentPageStyle={headerStyles.currentPageStyle}>
+											Home
+										</NavLink>
+									</li>
+									<li>
+										<NavLink href='/ordering' currentPageStyle={headerStyles.currentPageStyle}>
+											Ordering
+										</NavLink>
+									</li>
+									<li>
+										<NavLink href='/contact' currentPageStyle={headerStyles.currentPageStyle}>
+											Contact Us
+										</NavLink>
+									</li>
+								</ul>
+							</nav>
+							<nav className={headerStyles.accountMenu} aria-label='Account Menu'>
+								<ul>
+									<li>
+										<NavLink href='/account' currentPageStyle={headerStyles.currentPageStyle}>
+											Account
+										</NavLink>
+									</li>
+									<li style={{ display: isAuthLoading || !user ? 'none' : 'list-item' }}>
+										<SignOutButton />
+									</li>
+									<li style={{ display: isCartLoading || !getTotalItems() ? 'none' : 'list-item' }}>
+										<CheckoutButton />
+									</li>
+								</ul>
+							</nav>
+						</div>
+
+						<div className={headerStyles.logoContainer}>
+							<NavLink href='/' currentPageStyle={headerStyles.mainLogoLinkCurrent}>
+								<img className={headerStyles.mainLogo} src='/images/logos/appcentre-logo.svg' alt='AppCentre' />
+							</NavLink>
+						</div>
+
+						<nav aria-label='Products' className={headerStyles.productMenu}>
+							<ul ref={productUlRef} className={productNavIsMultiRow ? headerStyles.multiRowProductNav : ''}>
 								<li>
-									<NavLink href='/' currentPageStyle={headerStyles.currentPageStyle}>
-										Home
-									</NavLink>
+									<ProductDropdown hrefStart='/kerio-connect' navIsSingleRow={!productNavIsMultiRow}>
+										Kerio Connect
+									</ProductDropdown>
 								</li>
 								<li>
-									<NavLink href='/ordering' currentPageStyle={headerStyles.currentPageStyle}>
-										Ordering
-									</NavLink>
+									<DropdownMenu
+										title='Kerio Control'
+										linkData={[
+											{ linkText: 'Info', href: '/kerio-control', currentPageStyle: headerStyles.currentPageStyle },
+											{
+												linkText: 'Pricing',
+												href: '/kerio-control-pricing',
+												currentPageStyle: headerStyles.currentPageStyle,
+											},
+											{
+												linkText: 'Hardware',
+												href: '/kerio-control-box',
+												currentPageStyle: headerStyles.currentPageStyle,
+											},
+										]}
+										className={`${headerStyles.navDropdown} ${
+											productNavIsMultiRow ? headerStyles.navDropdownNoFixedOpen : ''
+										}`}
+										navIsSingleRow={!productNavIsMultiRow}
+									/>
 								</li>
 								<li>
-									<NavLink href='/contact' currentPageStyle={headerStyles.currentPageStyle}>
-										Contact Us
-									</NavLink>
+									<ProductDropdown hrefStart='/gfi-archiver' navIsSingleRow={!productNavIsMultiRow}>
+										GFI Archiver
+									</ProductDropdown>
 								</li>
-							</ul>
-						</nav>
-						<nav className={headerStyles.accountMenu} aria-label='Account Menu'>
-							<ul>
 								<li>
-									<NavLink href='/account' currentPageStyle={headerStyles.currentPageStyle}>
-										Account
-									</NavLink>
+									<ProductDropdown hrefStart='/gfi-helpdesk' navIsSingleRow={!productNavIsMultiRow}>
+										GFI HelpDesk
+									</ProductDropdown>
 								</li>
-								<li style={{ display: isAuthLoading || !user ? 'none' : 'list-item' }}>
-									<SignOutButton />
+								<li>
+									<ProductDropdown hrefStart='/gfi-mailessentials' navIsSingleRow={!productNavIsMultiRow}>
+										GFI MailEssentials
+									</ProductDropdown>
 								</li>
-								<li style={{ display: isCartLoading || !getTotalItems() ? 'none' : 'list-item' }}>
-									<CheckoutButton />
+								<li>
+									<ProductDropdown hrefStart='/gfi-languard' navIsSingleRow={!productNavIsMultiRow}>
+										GFI LanGuard
+									</ProductDropdown>
+								</li>
+								<li>
+									<DropdownMenu
+										title='Legacy Products'
+										linkData={[
+											{
+												linkText: 'Kerio Operator',
+												href: '/kerio-operator',
+												currentPageStyle: headerStyles.currentPageStyle,
+											},
+											{
+												linkText: 'GFI Unlimited',
+												href: '/gfi-unlimited',
+												currentPageStyle: headerStyles.currentPageStyle,
+											},
+										]}
+										className={`${headerStyles.navDropdown} ${
+											productNavIsMultiRow ? headerStyles.navDropdownNoFixedOpen : ''
+										}`}
+										navIsSingleRow={!productNavIsMultiRow}
+									/>
 								</li>
 							</ul>
 						</nav>
 					</div>
-
-					<div className={headerStyles.logoContainer}>
-						<NavLink href='/' currentPageStyle={headerStyles.mainLogoLinkCurrent}>
-							<img className={headerStyles.mainLogo} src='/images/logos/appcentre-logo.svg' alt='AppCentre' />
-						</NavLink>
-					</div>
-
-					<nav aria-label='Products' className={headerStyles.productMenu}>
-						<ul ref={productUlRef} className={productNavIsMultiRow ? headerStyles.multiRowProductNav : ''}>
-							<li>
-								<ProductDropdown hrefStart='/kerio-connect' navIsSingleRow={!productNavIsMultiRow}>
-									Kerio Connect
-								</ProductDropdown>
-							</li>
-							<li>
-								<DropdownMenu
-									title='Kerio Control'
-									linkData={[
-										{ linkText: 'Info', href: '/kerio-control', currentPageStyle: headerStyles.currentPageStyle },
-										{
-											linkText: 'Pricing',
-											href: '/kerio-control-pricing',
-											currentPageStyle: headerStyles.currentPageStyle,
-										},
-										{
-											linkText: 'Hardware',
-											href: '/kerio-control-box',
-											currentPageStyle: headerStyles.currentPageStyle,
-										},
-									]}
-									className={`${headerStyles.navDropdown} ${
-										productNavIsMultiRow ? headerStyles.navDropdownNoFixedOpen : ''
-									}`}
-									navIsSingleRow={!productNavIsMultiRow}
-								/>
-							</li>
-							<li>
-								<ProductDropdown hrefStart='/gfi-archiver' navIsSingleRow={!productNavIsMultiRow}>
-									GFI Archiver
-								</ProductDropdown>
-							</li>
-							<li>
-								<ProductDropdown hrefStart='/gfi-helpdesk' navIsSingleRow={!productNavIsMultiRow}>
-									GFI HelpDesk
-								</ProductDropdown>
-							</li>
-							<li>
-								<ProductDropdown hrefStart='/gfi-mailessentials' navIsSingleRow={!productNavIsMultiRow}>
-									GFI MailEssentials
-								</ProductDropdown>
-							</li>
-							<li>
-								<ProductDropdown hrefStart='/gfi-languard' navIsSingleRow={!productNavIsMultiRow}>
-									GFI LanGuard
-								</ProductDropdown>
-							</li>
-							<li>
-								<DropdownMenu
-									title='Legacy Products'
-									linkData={[
-										{
-											linkText: 'Kerio Operator',
-											href: '/kerio-operator',
-											currentPageStyle: headerStyles.currentPageStyle,
-										},
-										{
-											linkText: 'GFI Unlimited',
-											href: '/gfi-unlimited',
-											currentPageStyle: headerStyles.currentPageStyle,
-										},
-									]}
-									className={`${headerStyles.navDropdown} ${
-										productNavIsMultiRow ? headerStyles.navDropdownNoFixedOpen : ''
-									}`}
-									navIsSingleRow={!productNavIsMultiRow}
-								/>
-							</li>
-						</ul>
-					</nav>
+					<aside className='cartSection'>
+						<HeaderCartMenu />
+						<div className={headerStyles.gfiPartnerLogoContainer}>
+							<img src='/images/logos/gfi-gold-partner.svg' alt='GFI Gold Partner' />
+						</div>
+					</aside>
 				</div>
-				<aside className='cartSection'>
-					<HeaderCartMenu />
-					<div className={headerStyles.gfiPartnerLogoContainer}>
-						<img src='/images/logos/gfi-gold-partner.svg' alt='GFI Gold Partner' />
-					</div>
-				</aside>
-			</div>
-		</header>
+			</header>
+		</>
 	)
 }
 
